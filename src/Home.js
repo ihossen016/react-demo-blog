9564690d5bc3c705ec5aff018fe51008852e2400
ihossen016 <ihossen016@gmail.com>
2022-01-21 11:26:22 +0600

    Home component updated

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,13 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const {
-    data: blogs,
-    isPending,
-    error,
-  } = useFetch("http://localhost:8000/blogs");
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 };
